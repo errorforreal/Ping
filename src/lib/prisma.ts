6b99d3b38f7d28f2358e43e215770c4dd6fe2fc3
Krish Yadav <krishyadav2806@gmail.com>
2026-04-28 14:42:53 +0530

    handled frontend on cloud

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,4 +4,4 @@ import { PrismaPg } from "@prisma/adapter-pg";
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
 const prisma = new PrismaClient({ adapter });
 
-export default prisma;
+export default prisma ;
