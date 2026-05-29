83eac9604f3955595f30b8efc9f770b7e49449a0
Krish Yadav <krishyadav2806@gmail.com>
2026-05-29 21:16:40 +0530

    refactor(notification.ts) 1. use prisma upsert to prevent race condition on user creation 2. wrap db and queue calls in a unified try/catch block

--- src/controller/notification.ts
+++ src/controller/notification.ts
@@ -3,24 +3,28 @@ import prisma  from "../lib/prisma.js";
 import type { notificationPayload } from "../services/authenticatPayload.js";
 import { payloadSchema } from "../services/authenticatPayload.js";
 import { ZodError } from "zod";
+import { queueEvent } from "../queue/notification.queue.js";
 
 
 export async function handleNotify(req: Request, res: Response) {
-    const tenantId = req.tenantId;
-    if (!tenantId) return res.status(403).json({ message: "Tenant not found! Check Api key" });
+    const tenantid = req.tenantId;
+    if (!tenantid) return res.status(403).json({ message: "Tenant not found! Check Api key" });
 
     const tenant = await prisma.tenant.findUnique({
         where: {
-            id: tenantId
-        }
+            id: tenantid
+        },
+        select : {id : true}
     });
     if (!tenant) return res.status(403).json({ message: "Tenant not found! Check Api key" });
 
     const payload: notificationPayload = req.body;
 
 
+    let validPayload: notificationPayload;
+
     try {
-        const isValidPayload = payloadSchema.parse(payload);
+        validPayload = payloadSchema.parse(payload);
     } catch (error) {
         if (error instanceof ZodError) {
             return res.status(400).json({                   // 400 -> client sent bad input
@@ -31,8 +35,40 @@ export async function handleNotify(req: Request, res: Response) {
 
         return res.status(500).json({ message: "Internal Server error" });
     }
-    
 
-
+    try {
+        
+        const user = await prisma.user.upsert({
+            where: {
+                tenantId_externalUserId: {
+                    externalUserId: validPayload.user.id,
+                    tenantId : tenant.id
+                }
+            },
+            update: {},
+            create: {
+                externalUserId: validPayload.user.id,
+                tenantId : tenant.id
+            }
+        })
     
+        const notification = await prisma.notification.create({
+            data: {
+                tenantId: tenant.id,
+                userId: user.id ,
+                type: validPayload.notification.type,
+                title: validPayload.notification.title,
+                message: validPayload.notification.message,
+            },
+        });
+
+      
+        queueEvent({ userId : user.id, notificationId : notification.id, tenantId : tenant.id});
+    
+        return res.status(201).json({ id: notification.id });
+        
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" });
+    }
+
 }
